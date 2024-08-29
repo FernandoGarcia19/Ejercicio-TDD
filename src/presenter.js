95b26efc9d37735815b32cfaba7749766be77354
antonio <antonio.ontiveros@ucb.edu.bo>
2024-08-29 09:46:02 -0400

    test 2: add presentacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
-import totalizer from "./totalizador";
+import {totalizer_cantidad, totalizer_precio_item} from "./totalizador";
 
 const cantidad = document.querySelector("#cantidad");
+const precio_item = document.querySelector("#precio-item")
 
 const form = document.querySelector("#ventas-form");
 const div = document.querySelector("#resultado-div");
@@ -9,6 +10,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cant = Number.parseInt(cantidad.value);
+  const precioItem = Number.parseInt(precio_item.value);
 
-  div.innerHTML = "<p>" + totalizer(cant) + "</p>";
+  div.innerHTML = "<p>" + totalizer_cantidad(cant) + "</p>" + "<p>" + totalizer_precio_item(precioItem) + "</p";
 });
