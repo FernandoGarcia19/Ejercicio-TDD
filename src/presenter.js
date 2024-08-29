7200eca1113e622931e22be282b888a314ba4427
antonio <antonio.ontiveros@ucb.edu.bo>
2024-08-29 09:34:20 -0400

    interfaz

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,14 @@
-import sumar from "./sumador";
+import totalizer from "./totalizador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const cantidad = document.querySelector("#cantidad");
+
+const form = document.querySelector("#ventas-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const cant = Number.parseInt(cantidad.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + totalizer(cant) + "</p>";
 });
