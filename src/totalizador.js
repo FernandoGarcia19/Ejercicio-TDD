8deb2bc18f30cac224b9ebabad5c2673d30434f1
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-08-29 09:52:35 -0400

    Test 3: Mostrar el estado "CA"

--- src/totalizador.js
+++ src/totalizador.js
@@ -4,4 +4,8 @@ function totalizer_cantidad(cantidad){
 function totalizer_precio_item(precio){
     return precio;
 }
-export {totalizer_cantidad, totalizer_precio_item};
+function totalizer_impuesto(estado){
+    return estado;
+}
+
+export {totalizer_cantidad, totalizer_precio_item, totalizer_impuesto};
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,4 +1,4 @@
-import { totalizer_cantidad, totalizer_precio_item} from "./totalizador";
+import { totalizer_cantidad, totalizer_precio_item, totalizer_impuesto} from "./totalizador";
 
 describe("Prueba de  totalizador", () => {
     it("Test 1: Deberia mostrar la cantidad dada", () => {
@@ -6,5 +6,8 @@ describe("Prueba de  totalizador", () => {
     });
     it("Test 2: Deberia mostrar el precio dado", ()=> {
         expect(totalizer_precio_item(1)).toEqual(1);
+    });
+    it("Test 3: Deberia mostrar el Estado dado como CA", ()=> {
+        expect(totalizer_impuesto("CA")).toEqual("CA");
     });
 });
